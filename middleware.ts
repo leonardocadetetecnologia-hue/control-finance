928401dffafc7cf9bd5b70ef58cee82ff7d9cf0b
Leonardo Cadete <leonardocadete.tecnologia@gmail.com>
2026-03-30 15:02:01 -0300

    allow api routes in middleware

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,12 @@ import { auth } from '@/auth'
 
 export default auth((req) => {
   const isLoggedIn = Boolean(req.auth)
+  const isApiRoute = req.nextUrl.pathname.startsWith('/api')
   const isAuthPage = req.nextUrl.pathname.startsWith('/login')
+
+  if (isApiRoute) {
+    return
+  }
 
   if (isAuthPage && isLoggedIn) {
     return Response.redirect(new URL('/', req.nextUrl))
